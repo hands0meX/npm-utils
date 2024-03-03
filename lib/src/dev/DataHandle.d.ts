efbb95f2d6f7e1eec884da37087a6cb29e69faa4
hands0meX <1258849260@qq.com>
2024-03-03 22:06:51 +0800

    conf: npm upload change

--- lib/src/dev/DataHandle.d.ts
+++
@@ -1,3 +0,0 @@
-declare class H {
-    foo(): void;
-}
--- types/src/dev/ProjectUtils/EventBus.d.ts
+++ types/src/dev/ProjectUtils/EventBus.d.ts
@@ -1,12 +1,5 @@
-declare class Subscribe {
-    callBackFns: Function[];
-    eventsMap: Map<string, any>;
-    add(evName: string, callbackFn: Function): void;
-    get(evName: string): any;
-    del(evName: string): boolean;
-}
 export declare class EventBus {
-    subscribe: Subscribe;
+    private subscribe;
     constructor();
     fire(eventName: string, payload?: any): void;
     on(eventName: string, callbackFn: Function, scope?: this): void;
@@ -14,4 +7,3 @@ export declare class EventBus {
     once(eventName: string): void;
     hasEvent(eventName: string): boolean;
 }
-export {};
