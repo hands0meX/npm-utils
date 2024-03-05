c9fd4821d88701dbdac04965ef9a048e084e4c7c
handsomeX <1258849260@qq.com>
2024-03-05 13:33:11 +0800

    feat: change rollingRandomString

--- examples/test24.ts
+++ examples/test24.ts
@@ -1,10 +1,10 @@
 import { Console, T, Str, Reg } from '../src/index';
 
-const res = Str.rollingRandomString(123)
+const res = Str.rollingRandomString(12,undefined, true, true, true)
 Console.log(res)
 
-Console.log(Reg.isPhone('11111111111'))
-Console.log(Reg.isEmail("12@qq.com"))
-Console.log(Reg.isChinese("中文"))
-Console.log(Reg.isIdCard("123456789012345678"))
-Console.log(Reg.isUrl("http://www.baidu.com"))
+// Console.log(Reg.isPhone('11111111111'))
+// Console.log(Reg.isEmail("12@qq.com"))
+// Console.log(Reg.isChinese("中文"))
+// Console.log(Reg.isIdCard("123456789012345678"))
+// Console.log(Reg.isUrl("http://www.baidu.com"))
--- types/src/dev/DataUtils/Str.d.ts
+++ types/src/dev/DataUtils/Str.d.ts
@@ -1,8 +1,12 @@
 export declare class Str {
     /**
-     * 设置一个随机字符串
-     * @param length 设置长度, 默认为16
-     * @returns 随机字符串
+     * 创建一个指定长度的随机字符串
+     * @param length 长度
+     * @param useLowerCase 是否使用小写字母
+     * @param useUpperCase 是否使用大写字母
+     * @param useSpecialChars	是否使用特殊字符
+     * @param useNumbers 是否使用数字
+     * @returns
      */
-    static rollingRandomString(length?: number): string;
+    static rollingRandomString(length: any, useLowerCase?: boolean, useUpperCase?: boolean, useSpecialChars?: boolean, useNumbers?: boolean): string;
 }
